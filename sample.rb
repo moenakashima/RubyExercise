891db2ef03c94a6ebf836a1be634bf863fe8a50a
moenakashima <moenakashima.mn@gmail.com>
2023-01-18 21:36:59 +0900

    変数転換

--- sample.rb
+++ sample.rb
@@ -11,3 +11,10 @@ puts "Sam is " + 27.to_s + " years old."
 
 # to_i
 puts 100 + "200".to_i
+
+
+name = "A"
+weight = 50
+
+puts name + "さんの体重は" + weight.to_s + "kgです" 
+puts "#{name}さんの体重は#{weight*2}kgです" 
